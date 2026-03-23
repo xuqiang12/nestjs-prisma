392c67c0deca2442e5c991ad04cf749b7f2cf718
xuqiang <xuqiang@shwoody.com>
2026-03-23 19:13:17 +0800

    init

---
+++ src/app.module.ts
@@ -0,0 +1,29 @@
+import { Logger, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule, loggingMiddleware } from 'nestjs-prisma';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './modules/user/user.module';
+import config from './common/configs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [config] }),
+    PrismaModule.forRoot({
+      isGlobal: true,
+      prismaServiceOptions: {
+        middlewares: [
+          // configure your prisma middleware
+          loggingMiddleware({
+            logger: new Logger('PrismaMiddleware'),
+            logLevel: 'log',
+          }),
+        ],
+      },
+    }),
+    UserModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
